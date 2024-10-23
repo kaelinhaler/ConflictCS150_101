b417a798fe6b799f1adb57ba8c5562b9eec3a0ab
kaelinhaler <khaler@nic.edu>
2024-10-23 17:24:40 +0000

    from codespace

--- main.cpp
+++ main.cpp
@@ -1,11 +1,17 @@
 #include <iostream>
 using namespace std;
-
+void say(string word);
 
 int main()
 {
+    say("hello");
    cout<<"codespace\n";
 cout<<"hello world\n";
 
     return 0;
+}
+
+void say(string word)
+{
+    cout<<word;
 }
