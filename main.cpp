f64d9cb0f74d06e25ce085f8dfab1b077d0d2207
Kaelin Haler <khaler@nic.edu>
2024-10-23 10:20:18 -0700

    created hello world

--- main.cpp
+++ main.cpp
@@ -1,11 +1,17 @@
 #include <iostream>
 using namespace std;
 
+int add(int a, int b);
 
 int main()
 {
-
+    
     cout<<"hello world\n";
-
+    cout<<"5+7 = "<<add(5,7)<<endl;
     return 0;
+}
+
+int add(int a, int b)
+{
+    return a+b;
 }
